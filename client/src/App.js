5d2c3ef8d8ea3211d64f84f83b2182e70d33c4b7
Abiola Johnson <biola.johnson@yahoo.com>
2020-09-21 00:48:27 +0100

    react begins here

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,32 @@
 import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Layout/Navbar";
+import Landing from "./components/Layout/Landing";
 import "./App.css";
+import Register from "./components/Auth/Register";
+import Login from "./components/Auth/Login";
+import store from "./store";
+import { Provider } from "react-redux";
+import Alert from "./components/Layout/Alerts";
 
 const App = () => {
-  return <Fragment>Hi</Fragment>;
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <section className="container">
+            <Alert />
+            <Switch>
+              <Route path="/register" component={Register} />
+              <Route path="/login" component={Login} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
 };
 
 export default App;
---
+++ client/src/components/Layout/Landing.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Landing = () => {
+  return (
+    <section className="landing">
+      <div className="dark-overlay">
+        <div className="landing-inner">
+          <h1 className="x-large">Farmers market</h1>
+          <p className="lead">
+            Create a Farmer's profile/portfolio.{" "}
+            <span>Make your products known to the public!</span>
+          </p>
+          <div className="buttons">
+            <Link to="/register" className="btn btn-primary">
+              Sign Up
+            </Link>
+            <Link to="/login" className="btn btn-light">
+              Login
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Landing;
